94e8815c61fc8eb25e72735010fda404559863c6
sanchez9 <sanchez.stephanie@me.com>
2018-12-26 01:45:50 -0500

    updated test case

--- src/test/java/com/Syntax/practice/Test2.java
+++ src/test/java/com/Syntax/practice/Test2.java
@@ -1,5 +1,9 @@
 package com.Syntax.practice;
 
 public class Test2 {
-
+	public static void main(String[] args) {
+		System.out.println("Hello from test2");
+		System.out.println("I am making changes");
+		
+	}
 }
